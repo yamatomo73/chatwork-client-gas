f550446c41fc2419037e6a9e3c64fe93c23ee6a1
yamatomo73 <33831717+yamatomo73@users.noreply.github.com>
2018-11-03 13:30:18 +0900

    [fix] ゴミコード

--- client.js
+++ client.js
@@ -97,7 +97,6 @@
     */
     ChatWork.prototype._objectFilter = function(obj, keys) {
       var filter_obj = {};
-      var optional_keys = ['description', 'icon_preset', 'name'];
       for(var i = 0; i < keys.length; i++) {
         var key = keys[i];
         if (key in obj) {
